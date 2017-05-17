406e0d3ba0d3ae3780806d32df4ebda4d6a001c1
mattbey <matt.bey@gmail.com>
2017-05-16 19:03:06 -0500

    5/16/17

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,9 +2,9 @@ class Actor < ApplicationRecord
  #  - name: must be present; must be unique in combination with dob
  validates :name, :presence => true, :uniqueness => { :scope => :dob}
 
- has_many(:movies, :class_name => "Movie", :foreign_key => "actor_id")
- 
  has_many(:characters, :class_name => "Character", :foreign_key => "actor_id")
+ has_many(:movies, :class_name => "Movie", :foreign_key => "actor_id")
+
 
  # - dob: no rules
  # - bio: no rules
